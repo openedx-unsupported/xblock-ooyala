abe522e68aa1572eb9c959195642bd047320b976
Chris Dodge <cdodge@edx.org>
2014-04-08 21:54:03 -0400

    initial commit

---
+++ ooyala_player/ooyala_player.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports ###########################################################
+
+import logging
+
+from xblock.core import XBlock
+from xblock.fields import Scope, String
+from xblock.fragment import Fragment
+
+from .utils import render_template
+
+
+# Globals ###########################################################
+
+log = logging.getLogger(__name__)
+
+
+# Classes ###########################################################
+
+class OoyalaPlayerBlock(XBlock):
+    """
+    XBlock providing a video player for videos hosted on Brightcove
+    """
+    display_name = String(
+        display_name="Display Name",
+        help="This name appears in the horizontal navigation at the top of the page.",
+        scope=Scope.settings,
+        default="Drag and Drop"
+    )
+
+    content_id = String(
+        display_name="Content Id",
+        help="Identifier for the Content Id.",
+        scope=Scope.content
+    )
+
+    transcript_id = String(
+        display_name="Transcript Id",
+        help="Identifier for the 3Play Transcript.",
+        scope=Scope.content
+    )
+
+    def student_view(self, context):
+        """
+        Player view, displayed to the student
+        """
+
+        context = {
+            'title': self.display_name,
+            'content_id': self.content_id,
+            'transcript_id': self.transcript_id,
+        }
+
+        fragment = Fragment()
+        fragment.add_content(render_template('/templates/html/ooyala_player.html', context))
+        fragment.add_css_url(self.runtime.local_resource_url(self, 'public/css/ooyala_player.css'))
+        fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/ooyala_player.js'))
+
+        fragment.initialize_js('DragAndDropBlock')
+
+        return fragment
+
+    def studio_view(self, context):
+        """
+        Editing view in Studio
+        """
+        fragment = Fragment()
+        fragment.add_content(render_template('/templates/html/ooyala_player_edit.html', {
+            'self': self,
+        }))
+        fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/ooyala_player_edit.js'))
+
+        fragment.initialize_js('OolayaPlayerBlock')
+
+        return fragment
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Imports ###########################################################
+
+import os
+from setuptools import setup
+
+
+# Functions #########################################################
+
+def package_data(pkg, root_list):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for root in root_list:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+# Main ##############################################################
+
+setup(
+    name='xblock-ooyala-player',
+    version='0.1',
+    description='XBlock - Ooyala Video Player',
+    packages=['ooyala_player'],
+    install_requires=[
+        'XBlock',
+    ],
+    entry_points={
+        'xblock.v1': 'ooyala-player = ooyala_player:OoyalaBlock',
+    },
+    package_data=package_data("ooyala_player", ["static", "templates", "public"]),
+)
