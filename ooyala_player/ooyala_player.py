d3936679045cc79c4bbfb6569a39839dc2da2414
Chris Dodge <cdodge@edx.org>
2014-04-11 22:23:12 -0400

    get transcripts working

--- ooyala_player/ooyala_player.py
+++ ooyala_player/ooyala_player.py
@@ -37,10 +37,18 @@ class OoyalaPlayerBlock(XBlock):
         default='hkdjY5bDq8v1L5e1S4NZ3nvIKi8ADt1n'
     )
 
-    transcript_id = String(
-        display_name="Transcript Id",
-        help="Identifier for the 3Play Transcript.",
-        scope=Scope.content
+    transcript_file_id = String(
+        display_name="3Play Transcript Id",
+        help="Identifier for the 3Play Transcript File",
+        scope=Scope.content,
+        default='375889'
+    )
+
+    transcript_project_id = String(
+        display_name="3Play Transcript Project Id",
+        help='Identifier for the 3Play Transcript Project',
+        scope=Scope.content,
+        default='12901'
     )
 
     player_id = '635104fd644c4170ae227af2de27deab'
@@ -53,7 +61,8 @@ class OoyalaPlayerBlock(XBlock):
         context = {
             'title': self.display_name,
             'content_id': self.content_id,
-            'transcript_id': self.transcript_id,
+            'transcript_id': self.transcript_file_id,
+            'transcript_project_id': self.transcript_project_id,
             'player_id': self.player_id,
         }
 
@@ -65,13 +74,23 @@ class OoyalaPlayerBlock(XBlock):
 
         #fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/vendor/jquery-1_10_1.js'))
         fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/vendor/jquery-ui.js'))
-        fragment.add_javascript_url('//player.ooyala.com/v3/' + self.player_id + '?platform=html5-priority')
+
+        player_url = '//player.ooyala.com/v3/{0}?platform=html5-priority'.format(self.player_id)
+
+        fragment.add_javascript_url(player_url)
         fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/vendor/speed_plugin.js'))
         fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/vendor/popcorn.js'))
         fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/vendor/underscore.js'))
         fragment.add_javascript_url(self.runtime.local_resource_url(self, 'public/js/ooyala_player.js'))
 
-        #fragment.add_javascript_url('//static.3playmedia.com/p/projects/12901/files/375889/embed.js?plugin=transcript&settings=width:640,height:240,skin:frost,can_collapse:true,collapse_onload:true,can_print:true,can_download:true,scan_view:true&player_type=ooyala&player_id=635104fd644c4170ae227af2de27deab')
+        transcript_js_url = '''
+                            //static.3playmedia.com/p/projects/{0}/files/{1}/embed.js?
+                            plugin=transcript&settings=width:640,height:240,skin:frost,
+                            can_collapse:true,collapse_onload:true,can_print:true,can_download:true,
+                            scan_view:true&player_type=ooyala&player_id={2}
+                            '''.format(self.transcript_project_id, self.transcript_file_id, self.player_id)
+
+        fragment.add_javascript_url(transcript_js_url)
 
         fragment.initialize_js('OoyalaPlayerBlock')
 
--- ooyala_player/public/js/ooyala_player.js
+++ ooyala_player/public/js/ooyala_player.js
@@ -1,7 +1,13 @@
 function OoyalaPlayerBlock(runtime, element) {
     OO.ready(function() {
+
         var content_id = $('.ooyalaplayer', element).data('content-id');
         var player_id = $('.ooyalaplayer', element).data('player-id');
+        var transcript_id = $('.ooyalaplayer', element).data('transcript-id');
+
+        // move the transcript widget into the right place in the DOM
+        // after it is injected by the 3Play JS code
+        $('#transcript_'+transcript_id).appendTo('.transcript-container');
 
          window[player_id] = OO.Player.create('ooyalaplayer', content_id, {
            /*onCreate: window.onCreate,*/
@@ -9,9 +15,4 @@ function OoyalaPlayerBlock(runtime, element) {
          });
     });
 
-    var script = document.createElement( 'script' );
-    script.type = 'text/javascript';
-    script.src = '//static.3playmedia.com/p/projects/12901/files/375889/embed.js?plugin=transcript&settings=width:640,height:240,skin:frost,can_collapse:true,collapse_onload:true,can_print:true,can_download:true,scan_view:true&player_type=ooyala&player_id=635104fd644c4170ae227af2de27deab';
-
-    $('.metadata', element).append(script);
 }
