867f18d2705f799309feb64847974193f0371360
dragonfi <david.gabor.bodor@gmail.com>
2014-08-11 12:59:42 +0200

    Remove redundant user_id and component_id fields from events

--- ooyala_player/ooyala_player.py
+++ ooyala_player/ooyala_player.py
@@ -235,14 +235,10 @@ class OoyalaPlayerBlock(XBlock):
 
     @XBlock.json_handler
     def publish_event(self, data, suffix=''):
-
         try:
             event_type = data.pop('event_type')
         except KeyError as e:
             return {'result': 'error', 'message': 'Missing event_type in JSON data'}
-
-        data['component_id'] = self.scope_ids.usage_id
-        data['user_id'] = self.runtime.user_id
 
         self.runtime.publish(self, event_type, data)
         return {'result':'success'}
