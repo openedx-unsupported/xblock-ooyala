c5d8946214fef5c6250b3fd85d3e6e358a5c12eb
zamanafzal <zamanafzal@gmail.com>
2018-11-29 01:41:52 +0500

    Remove bit_wrapper and discovery_api as this was causing issue on IE

--- ooyala_player/ooyala_player.py
+++ ooyala_player/ooyala_player.py
@@ -187,7 +187,6 @@ class OoyalaPlayerMixin(object):
 
         JS_URLS = [
             self.local_resource_url(self, 'public/build/player_all.min.js'),
-            self.local_resource_url(self, 'public/js/vendor/ooyala/bit_wrapper.js'),
             '//p3.3playmedia.com/p3sdk.current.js',
             self.local_resource_url(self, 'public/js/ooyala_player.js'),
         ]
