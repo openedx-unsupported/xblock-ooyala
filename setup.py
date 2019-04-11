340511a6f3c25c175e2b88c865bb45185eb45f0d
Naeem Ilyas <naeem-ilyas@live.com>
2019-04-11 07:17:45 +0000

    version bump

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ BLOCKS_CHILDREN = [
 
 setup(
     name='xblock-ooyala-player',
-    version='2.0.23',
+    version='2.0.24',
     description='XBlock - Ooyala Video Player',
     packages=['ooyala_player'],
     install_requires=[
