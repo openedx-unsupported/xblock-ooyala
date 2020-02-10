bbb15787c8ce95c69d5c8f5a6f764633300d11d2
Naeem Ilyas <naeem-ilyas@live.com>
2020-02-10 15:17:16 +0500

    MCKIN-13055 - default video id changed (#109)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ BLOCKS_CHILDREN = [
 
 setup(
     name='xblock-ooyala-player',
-    version='3.0.0',
+    version='3.0.1',
     description='XBlock - Ooyala Video Player',
     packages=['ooyala_player'],
     install_requires=[
