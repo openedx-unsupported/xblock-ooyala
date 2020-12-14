0507750e370a2cada12551d2d31d528c01456598
Nasir Hussain <nasirhjafri@gmail.com>
2020-12-14 18:22:03 +0500

    Release v1.58.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ BLOCKS_CHILDREN = [
 
 setup(
     name='xblock-ooyala-player',
-    version='4.1.2',
+    version='4.1.3',
     description='XBlock - Ooyala Video Player',
     packages=['ooyala_player'],
     install_requires=[
